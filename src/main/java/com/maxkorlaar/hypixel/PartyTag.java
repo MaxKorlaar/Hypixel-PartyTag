69da017b6d506988a2450ab118c84ac9a81391f5
Max <max@maxkorlaar.com>
2016-03-06 20:52:46 +0100

    Improve log messages

--- src/main/java/com/maxkorlaar/hypixel/PartyTag.java
+++ src/main/java/com/maxkorlaar/hypixel/PartyTag.java
@@ -58,7 +58,7 @@ import java.util.*;
 @Mod(modid = PartyTag.MODID, version = PartyTag.VERSION)
 public class PartyTag {
     public static final String MODID = "PartyTag";
-    public static final String VERSION = "1.1.0";
+    public static final String VERSION = "1.1.1";
     private static Collection<String> partyMembers = new ArrayList<String>();
     public static Map<UUID, String> stringCache = new HashMap<java.util.UUID, String>();
     long waitUntil = System.currentTimeMillis();
@@ -131,10 +131,9 @@ public class PartyTag {
         }
         updates++;
         Minecraft mc = Minecraft.getMinecraft();
-        logger.info("Now sending the /p list command!");
+        logger.info(logPrefix + "Now sending the /p list command!");
 
         mc.thePlayer.sendChatMessage("/party list");
-        //mc.thePlayer.sendChatMessage("/pchat Sending list command - IF THIS GETS SPAMMED, SOMETHING IS WRONG, SORRY");
     }
 
     private void clearParty() {
@@ -165,7 +164,7 @@ public class PartyTag {
                         memberName = rawMemberStringTrimmed;
                     }
                     partyMembers.add(memberName);
-                    logger.info("[PARTY] PARTY MEMBER:" + memberName);
+                    logger.info(logPrefix + "Found party member: " + memberName);
                 }
             }
         } else if (chatMessage.startsWith("You are not in a party") || chatMessage.startsWith("The party was disbanded") || chatMessage.startsWith("You have been kicked from the party")) {
